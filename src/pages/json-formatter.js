c0d21478308a78020c001df7b8ec0e2f0e32b5b6
ayah.refai@digitinary.com <ayah.refai@digitinary.com>
2024-07-27 19:40:25 +0300

    json

--- src/pages/json-formatter.js
+++ src/pages/json-formatter.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 import Editor from '@monaco-editor/react';
+import 'react-toastify/dist/ReactToastify.css';
 import styles from '../css/style.module.css';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 export default function JsonFormatter() {
 
@@ -9,32 +12,37 @@ export default function JsonFormatter() {
 
   const formatJSON = () => {
     try {
-      const parsedJSON = JSON.parse(input);
-      const prettyJSON = JSON.stringify(parsedJSON, null, 2);
-      setInput(prettyJSON);
+      if (input != null && input != undefined && input !== "") {
+        const parsedJSON = JSON.parse(input);
+        const prettyJSON = JSON.stringify(parsedJSON, null, 2);
+        setInput(prettyJSON);
+      }
     } catch (error) {
-      // setInput('Invalid JSON');
+      toast.error("Invalid JSON")
     }
   };
 
   const jsonOneLine = () => {
     try {
-      const jsonObject = JSON.parse(input);
-      const singleLineJsonString = JSON.stringify(jsonObject);
-      setInput(singleLineJsonString);
+      if (input != null && input != undefined && input !== "") {
+
+        const jsonObject = JSON.parse(input);
+        const singleLineJsonString = JSON.stringify(jsonObject);
+        setInput(singleLineJsonString);
+      }
     } catch (error) {
-      // setInput('Invalid JSON');
+      toast.error("Invalid JSON")
     }
   };
 
   return (
     <Layout>
       <div className={`${styles.jsonFormatter}`}>
-      <div>
-      <button className={`${styles.btn} ${styles.colorBtn}`} onClick={formatJSON}>Format JSON</button>
-      <button className={`${styles.btn} ${styles.colorBtn}`} onClick={jsonOneLine}>One Line JSON</button>
-      </div>
-      <br />
+        <div>
+          <button className={`${styles.btn} ${styles.colorBtn}`} onClick={formatJSON}>Format JSON</button>
+          <button className={`${styles.btn} ${styles.colorBtn}`} onClick={jsonOneLine}>One Line JSON</button>
+        </div>
+        <br />
         <Editor
           language="json"
           value={input}
@@ -45,6 +53,7 @@ export default function JsonFormatter() {
           theme='vs-dark'
         />
       </div>
+      <ToastContainer />
     </Layout>
   );
 }
